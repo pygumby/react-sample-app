845ff7f0b32a8f873bfb41325168439d1ead6beb
Lucas Konstantin Bärenfänger <18075314+lkbaerenfaenger@users.noreply.github.com>
2024-06-12 22:51:44 +0000

    Lesson 2: Creating Add Task Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 
+// Custom components
+import CustomForm from './components/CustomForm'
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const addTask = (task) => {
+    console.log(task)
+  }
+
   return (
-    <div className="App">
+    <div className="container">
+      <header>
+        <h1>React Sample App</h1>
+      </header>
+      <CustomForm addTask={addTask} />
     </div>
   )
 }
